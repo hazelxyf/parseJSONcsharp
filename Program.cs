221d2fa35d247828b96bdccf65115b1b84137e5d
hazelxyf <48450526+hazelxyf@users.noreply.github.com>
2019-07-19 15:13:35 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseJSON
+{
+    class Program
+    {
+        private static void dateAndPrices(string startDate, string endDate)
+        {
+            var client = new WebClient();
+            var text = client.DownloadString("https://jsonmock.hackerrank.com/api/stocks");
+
+            RootObject stockPrices = JsonConvert.DeserializeObject<RootObject>(text);
+
+            DateTime dateStartDate = DateTime.Parse(startDate);
+            DateTime dateEndDate = DateTime.Parse(endDate);
+            
+            for (int i = 0; i < stockPrices.data.Count; i++)
+            {
+                DateTime date = DateTime.Parse(stockPrices.data[i].date);
+                if (date >= dateStartDate && date <= dateEndDate)
+                {
+                    Console.WriteLine("Date: " + stockPrices.data[i].date);
+                    Console.WriteLine("Open Price: " + stockPrices.data[i].open);
+                    Console.WriteLine("Close Price: " + stockPrices.data[i].close);
+
+                    Console.WriteLine();
+                }
+            }
+
+            Console.ReadLine();
+           
+            
+        }
+
+        static void Main(string[] args)
+        {
+            //5-January-2000
+            var sDate = "7-January-2000";
+            var eDate = "15-January-2000";
+
+            dateAndPrices(sDate, eDate);
+        }
+    }
+}
+
---
+++ StockPrices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParseJSON
+{
+    public class StockPrices
+    {
+        public string date { get; set; }
+        public double open { get; set; }
+        public double high { get; set; }
+        public double low { get; set; }
+        public double close { get; set; }
+    }
+
+    public class RootObject
+    {
+        public int page { get; set; }
+        public int per_page { get; set; }
+        public int total { get; set; }
+        public int total_pages { get; set; }
+        public List<StockPrices> data { get; set; }
+    }
+}
